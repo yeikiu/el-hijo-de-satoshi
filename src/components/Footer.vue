fefe1a4dc0ab44e977486626199cfd543514eddb
Yeikiu <jqocon@gmail.com>
2020-09-02 17:58:31 +0100

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="text-center mt-5 py-2" v-bind:style="`background-color: ${VUE_APP_FOOTER_BG_CSS_COLOR}; color: ${VUE_APP_FOOTER_TEXT_CSS_COLOR};`">
-    <a href="#/" class="text-reset"><p class="m-0 p-0">vue-base-blog v{{version}}</p></a>
+    <p class="m-0 p-0"><a href="#/" class="text-reset">EHDS v{{version}}</a> | Powered by <a href="https://github.com/yeikiu/vue-base-blog" target="_blank" class="text-reset">vue-base-blog</a></p>
     <p class="m-0 p-0">Created with ❤️ by <a href="https://github.com/yeikiu/" target="_blank" class="text-reset">JQ</a> © {{ new Date().toDateString().slice(-4) }}</p>
   </div>
 </template>
