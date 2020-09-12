cfdeb2e07312d47f5d7377ece99ab7c52c9ef8cf
Yeikiu <jqocon@gmail.com>
2020-09-12 03:45:38 +0100

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-light" v-bind:style="`background-color: ${VUE_APP_NAVBAR_BG_CSS_COLOR}; color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`">
-    <a class="navbar-brand" href="#/" v-bind:style="`color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`">🏠 El Hijo de Satoshi</a>
+    <a class="navbar-brand" href="#/" v-bind:style="`color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`">👶 El Hijo de Satoshi 🕶️</a>
     <button :class="`navbar-toggler ${collapseMenu ? 'collapsed' : ''}`" @click="collapseMenu = !collapseMenu" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" :aria-expanded="!collapseMenu" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon" v-bind:style="`background-color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`"></span>
     </button>
@@ -9,7 +9,7 @@
       <ul class="pr-3 navbar-nav ml-auto">
         <li :class="`nav-item dropdown ${showDropdown ? 'show' : ''}`">
           <a v-bind:style="`color: ${VUE_APP_NAVBAR_TEXT_CSS_COLOR};`" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" href="" @click.prevent="showDropdown = !showDropdown" data-toggle="dropdown" aria-haspopup="true" :aria-expanded="showDropdown">
-            Blog Sections
+            Sections
           </a>
           <div :class="`dropdown-menu ${showDropdown ? 'show' : ''}`" aria-labelledby="navbarDropdownMenuLink">
             <a v-for="(numPosts, section) in sections" :key="section" class="dropdown-item text-capitalize" :href="`#/${section === 'all' ? '' : section}`">{{section}} ({{numPosts}})</a>
